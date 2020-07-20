69f8fc6a584968ded3f256e326d3b27b53b0f32e
abhishekG <ab25gaurav@gmail.com>
2020-07-20 19:47:40 +0530

    first commit

--- abhishekPort/particles.js
+++ abhishekPort/particles.js
@@ -1,11 +1,5 @@
-/* -----------------------------------------------
-/* Author : Vincent Garreau  - vincentgarreau.com
-/* MIT license: http://opensource.org/licenses/MIT
-/* Demo / Generator : vincentgarreau.com/particles.js
-/* GitHub : github.com/VincentGarreau/particles.js
-/* How to use? : Check the GitHub README
-/* v2.0.0
-/* ----------------------------------------------- */
+
+/* -------------------js file of particles included in home page----------------------- */
 
 var pJS = function(tag_id, params){
 
